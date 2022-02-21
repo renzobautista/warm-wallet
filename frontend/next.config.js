1ce9571d6e4fee0163b3af1f8bc8dcdeb791311a
Renzo Bautista <renzobautista2@gmail.com>
2022-02-21 17:02:01 -0500

    Add check for chain id.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    CHAIN_ID: process.env.CHAIN_ID,
+    CHAIN_NAME: process.env.CHAIN_NAME,
+    FACTORY_CONTRACT_ADDRESS: process.env.FACTORY_CONTRACT_ADDRESS
+  }
 }
-
 module.exports = nextConfig
